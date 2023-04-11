ee6b0e3dc338f0e628d6159554b6ef0dc0c2c39f
den669 <71308629+den669@users.noreply.github.com>
2023-04-11 21:33:26 +0300

    Add files via upload

---
+++ laba11/ClassLibrary1/Class1.cs
@@ -0,0 +1,60 @@
+﻿public abstract class InformationHandler
+{
+    protected InformationHandler NextHandler; // Наступний обробник в ланцюжку
+
+    public void SetNextHandler(InformationHandler handler)
+    {
+        NextHandler = handler;
+    }
+
+    public abstract void HandleRequest(string request); // Метод обробки запиту
+
+}
+
+// Конкретний обробник, що відповідає за перевірку наявності інформації на локальному сервері
+public class LocalServerHandler : InformationHandler
+{
+    public override void HandleRequest(string request)
+    {
+        if (IsInformationAvailableLocally(request))
+        {
+            Console.WriteLine($"Інформація '{request}' знайдена на локальному сервері.");
+        }
+        else
+        {
+            Console.WriteLine($"Інформацію '{request}' не знайдено на локальному сервері.");
+            NextHandler?.HandleRequest(request); // Передаємо запит на наступний обробник в ланцюжку
+        }
+    }
+
+    private bool IsInformationAvailableLocally(string request)
+    {
+        // Логіка перевірки наявності інформації на локальному сервері
+        // Повертає true, якщо інформація знайдена, інакше - false
+        return false;
+    }
+}
+
+// Конкретний обробник, що відповідає за пошук інформації на віддаленому сервері
+public class RemoteServerHandler : InformationHandler
+{
+    public override void HandleRequest(string request)
+    {
+        if (IsInformationAvailableRemotely(request))
+        {
+            Console.WriteLine($"Інформація '{request}' знайдена на віддаленому сервері.");
+        }
+        else
+        {
+            Console.WriteLine($"Інформацію '{request}' не знайдено на віддаленому сервері.");
+            // Це останній обробник в ланцюжку, тому не викликаємо NextHandler
+        }
+    }
+
+    private bool IsInformationAvailableRemotely(string request)
+    {
+        // Логіка пошуку інформації на віддаленому сервері
+        // Повертає true, якщо інформація знайдена, інакше - false
+        return true;
+    }
+}
---
+++ laba11/laba11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Абстрактний клас, що представляє базовий об'єкт ланцюжка
+
+
+// Клієнтський код
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Створюємо об'єкти обробників
+        var localServerHandler = new LocalServerHandler();
+        var remoteServerHandler = new RemoteServerHandler();
+        localServerHandler.SetNextHandler(remoteServerHandler);
+        localServerHandler.HandleRequest("Запит на інформацію");
+    }
+}
